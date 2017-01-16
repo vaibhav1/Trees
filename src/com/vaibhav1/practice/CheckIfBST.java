2d62192e56448a4894e7373983408b0c224f2e38
vaibhav1 <vaibhav2008in@gmail.com>
2017-01-16 15:09:15 +0530

    New repo started. Tree data strucuture problems

---
+++ src/com/vaibhav1/practice/CheckIfBST.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.vaibhav1.practice;
+
+/**
+ * @author vshukla
+ *
+ */
+public class CheckIfBST {
+	
+	
+	class Node {
+		Node left;
+		Node right;
+		int data;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
